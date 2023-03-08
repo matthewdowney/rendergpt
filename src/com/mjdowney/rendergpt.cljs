180a3c3ac262dc6365c519a3d375c3d0556f9d73
Matthew Downey <matthewdowney20@gmail.com>
2023-03-08 10:28:08 -0500

    Render GPT HTML output in an iframe, clumsily floated front and center

--- src/com/mjdowney/rendergpt.cljs
+++ src/com/mjdowney/rendergpt.cljs
@@ -11,18 +11,54 @@
 (defn build-render-button []
   (crate/html [:button.flex.ml-auto.gap-2 "Render"]))
 
+(defn get-gpt-response-code-blocks []
+  (js/document.getElementsByClassName "bg-black mb-4 rounded-md"))
+
+(defn code-block-type [code-block-ele]
+  (some-> (.getElementsByTagName code-block-ele "span") first .-innerText))
+
+(defn set-code-block-type! [code-block-ele type]
+  (when-let [e (some-> (.getElementsByTagName code-block-ele "span") first)]
+    (set! (.-innerText e) type)))
+
+(defn code-block-source [code-block-ele]
+  (some-> (.getElementsByTagName code-block-ele "code") first .-innerText))
+
+(defn add-to-code-block-title-bar! [code-block-ele new-ele]
+  (if-let [existing-button (first (.getElementsByTagName code-block-ele "button"))]
+    (.insertBefore (.-parentNode existing-button) new-ele existing-button)
+    (.appendChild (.-parentElement code-block-ele) new-ele)))
+
+(defn iframe [srcdoc]
+  (let [width (/ (.-clientWidth (.-body js/document)) 2)
+        height (/ (.-clientHeight (.-body js/document)) 2)]
+    (crate/html
+      [:iframe
+       {:srcdoc srcdoc
+        :style {:position "fixed"
+                :top "50%"
+                :left "50%"
+                :width (str width "px")
+                :height (str height "px")
+                :transform "translate(-50%, -50%)"
+                :border "1px solid steelblue"
+                :border-radius "5px"
+                :background-color "white"}}])))
+
+
 (defn on-mutation [mutation-records _observer]
   (js/console.log "Mutation observed" mutation-records)
-  (doseq [ele (js/document.getElementsByClassName "bg-black mb-4 rounded-md")]
-    (when-let [code-block-type (some-> (.getElementsByTagName ele "span") first)]
-      (when (= (.-innerText code-block-type) "html")
-        (js/console.log "Found html" code-block-type)
-        (set! (.-innerText code-block-type) "html+")
+  (doseq [ele (get-gpt-response-code-blocks)]
+    (when (= (code-block-type ele) "html")
+      (set-code-block-type! ele "html+")
 
-        (let [render-button (build-render-button)]
-          (if-let [existing-button (first (.getElementsByTagName ele "button"))]
-            (.insertBefore (.-parentNode existing-button) render-button existing-button)
-            (.appendChild (.-parentElement code-block-type) render-button)))))))
+      (let [render-button (build-render-button)]
+        (.addEventListener render-button "click"
+          (fn [_e]
+            (js/console.log "Code:" (code-block-source ele))
+            (.appendChild js/document.body (iframe (code-block-source ele)))))
+
+        (add-to-code-block-title-bar! ele render-button)))))
 
 (defn register-on-mutation [f]
   (let [observer (js/MutationObserver. f)]
