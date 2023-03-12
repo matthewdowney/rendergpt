46852776350233ca9400a63e29c36d8c18d98dc2
Matthew Downey <matthewdowney20@gmail.com>
2023-03-11 20:54:18 -0500

    Improve dropdown selection for code sources to include

--- src/com/mjdowney/rendergpt.cljs
+++ src/com/mjdowney/rendergpt.cljs
@@ -21,6 +21,22 @@
 (defn code-block-source [code-block-ele]
   (some-> (.getElementsByTagName code-block-ele "code") first .-innerText))
 
+(defn prompt-for-code-block
+  "Given ChatGPT's response code, get the text of the preceding user prompt."
+  [code-block-ele]
+  (let [chat-parent-sel "flex flex-col items-center text-sm dark:bg-gray-800"
+        chat-parent (first (js/document.getElementsByClassName chat-parent-sel))
+        user-msgs (reverse (take-nth 2 (.-children chat-parent)))]
+    (when-let [msg
+               (first
+                 (drop-while
+                   (fn [user-chat]
+                     ; Drop while the code block is not after the user msg
+                     (= (.compareDocumentPosition code-block-ele user-chat)
+                        (.-DOCUMENT_POSITION_FOLLOWING js/Node)))
+                   user-msgs))]
+      (.-textContent msg))))
+
 (defn add-to-code-block-title-bar! [code-block-ele new-ele]
   (if-let [existing-button (first (.getElementsByTagName code-block-ele "button"))]
     (.insertBefore (.-parentNode existing-button) new-ele existing-button)
@@ -31,27 +47,61 @@
 
 (def all-code-blocks
   "All code blocks on the page, in order, with their types and source code."
-  (r/atom [#_{:code "<div ...>" :type "html"}]))
+  (r/atom [#_{:code "<div ...>" :type "html" :desc "user message to chatgpt"}]))
 
-(defn code-block-options [initial-selection-idx all-code-blocks]
+(defn code-selection-options [initial-selection-idx all-code-blocks]
   (into []
     (map-indexed
-      (fn [idx {:keys [type]}]
+      (fn [idx {:keys [type desc]}]
         {:label (str type " @" (if (= idx initial-selection-idx) "here" idx))
+         :desc desc
          :value idx}))
     all-code-blocks))
+
+(defn selection-option-renderer
+  "Customize the dropdown component for selecting code blocks to include to
+  also display a preview of the prompt that produced each of the code blocks."
+  [props]
+  (let [{:keys [checked option onClick disabled]} (js->clj props :keywordize-keys true)]
+    (r/as-element
+      [:div {:class (str "item-renderer" (if disabled " disabled" ""))}
+       [:input {:type "checkbox"
+                :checked checked
+                :disabled disabled
+                :onChange (fn [e] (onClick e))
+                :tabIndex -1}]
+       [:div.code-selection {:style {:display "grid" :padding 0 :margin 0}}
+        [:span (:label option)]
+        [:span.code-selection-desc (:desc option)]]])))
+
+(defn filter-options
+  "Filter the options in a multi-select dropdown for search term inclusion in
+  either the :label or the :desc."
+  [options search-term]
+  (if (seq search-term)
+    (let [search-term (string/lower-case search-term)]
+      (.filter options
+        (fn [option]
+          (or
+            (string/includes? (.-label ^js option) search-term)
+            (string/includes?
+              (string/lower-case (.-desc ^js option))
+              search-term)))))
+    options))
 
 (defn sources-dropdown
   "A multi-select dropdown for selecting which of ChatGPT's code sources to
   include, beyond the selected code block."
   [initial-selection-idx selected]
-  (let [options (code-block-options initial-selection-idx @all-code-blocks)]
+  (let [options (code-selection-options initial-selection-idx @all-code-blocks)]
     [:> MultiSelect
-     {:options (clj->js options)
+     {:options (clj->js (reverse options))
       :overrideStrings #js {:selectSomeItems "Sources..."
                             :allItemsAreSelected "All sources selected"}
+      :filterOptions filter-options
+      :ItemRenderer selection-option-renderer
       :className "dark"
-      :hasSelectAll true
+      :hasSelectAll false
       :disableSearch false
       :onChange (fn [e]
                   (js/console.log "Selected:" e)
@@ -72,6 +122,8 @@
     (fn [code-block-idx]
       [:div.p4.overflow-y-auto.font-sans {:style {:min-height "500px"}}
        [sources-dropdown code-block-idx selected]
+       ;; TODO: Perhaps this needs to be a type 3 component, and re-initialize
+       ;;       completely when sources change.
        [:iframe
         {:srcDoc (->> @selected
                       (map (fn [idx] (code-block-html (nth @all-code-blocks idx))))
@@ -121,7 +173,9 @@
 
       (when (>= idx n-registered)
         (swap! all-code-blocks conj
-          {:code (code-block-source ele) :type (code-block-type ele)}))
+          {:code (code-block-source ele)
+           :type (code-block-type ele)
+           :desc (prompt-for-code-block ele)}))
 
       (when (= (code-block-type ele) "html")
         (set-code-block-type! ele "html+")
