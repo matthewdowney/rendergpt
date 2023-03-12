abd5814586e2f563a08b566a661f04ae910c8ce2
Matthew Downey <matthewdowney20@gmail.com>
2023-03-11 19:27:41 -0500

    Combine iFrame sources according to user's selection

--- src/com/mjdowney/rendergpt.cljs
+++ src/com/mjdowney/rendergpt.cljs
@@ -1,5 +1,6 @@
 (ns com.mjdowney.rendergpt
-  (:require [crate.core :as crate]
+  (:require [clojure.string :as string]
+            [crate.core :as crate]
             [reagent.dom :as rdom]
             [reagent.core :as r]
             ["react-multi-select-component" :refer (MultiSelect)]))
@@ -28,49 +29,70 @@
 ;;; (2) A React component replacing the code block contents with an iframe for
 ;;; displaying the result of evaling / rendering the code.
 
+(def all-code-blocks
+  "All code blocks on the page, in order, with their types and source code."
+  (r/atom [#_{:code "<div ...>" :type "html"}]))
+
+(defn code-block-options [initial-selection-idx all-code-blocks]
+  (into []
+    (map-indexed
+      (fn [idx {:keys [type]}]
+        {:label (str type " @" (if (= idx initial-selection-idx) "here" idx))
+         :value idx}))
+    all-code-blocks))
+
 (defn sources-dropdown
   "A multi-select dropdown for selecting which of ChatGPT's code sources to
   include, beyond the selected code block."
-  []
-  (let [selected (r/atom [])]
-    (fn []
-      [:> MultiSelect
-       {:options (clj->js
-                   [{:label "Option 1" :value "option1"}
-                    {:label "Option 2" :value "option2"}
-                    {:label "Option 3" :value "option3"}])
-        :overrideStrings #js {:selectSomeItems "Sources..."
-                              :allItemsAreSelected "All sources selected"}
-        :className "dark"
-        :hasSelectAll false
-        :disableSearch true
-        :onChange (fn [e] (js/console.log "Selected:" (reset! selected (js->clj e))))
-        :value @selected}])))
+  [initial-selection-idx selected]
+  (let [options (code-block-options initial-selection-idx @all-code-blocks)]
+    [:> MultiSelect
+     {:options (clj->js options)
+      :overrideStrings #js {:selectSomeItems "Sources..."
+                            :allItemsAreSelected "All sources selected"}
+      :className "dark"
+      :hasSelectAll true
+      :disableSearch false
+      :onChange (fn [e]
+                  (js/console.log "Selected:" e)
+                  (reset! selected (mapv #(.-value %) e)))
+      :value (mapv #(nth options %) @selected)}]))
 
-(defn rendergpt [srcdoc]
-  [:div.p4.overflow-y-auto.font-sans {:style {:min-height "500px"}}
-   [sources-dropdown]
-   [:iframe
-    {:srcDoc srcdoc
-     :style {:position "relative"
-             :width "100%"
-             :min-height "500px"
-             :border "1px solid steelblue"
-             :border-radius "5px"
-             :background-color "white"}}]])
+(defn code-block-html [{:keys [code type]}]
+  (case type
+    "html" code
+    "javascript" (str "<script>\n" code "\n</script>")
+    "css" (str "<style>\n" code "\n</style>")
+
+    ; if the code type is unknown, just embed it directly
+    code))
+
+(defn rendergpt [code-block-idx]
+  (let [selected (r/atom [code-block-idx])]
+    (fn [code-block-idx]
+      [:div.p4.overflow-y-auto.font-sans {:style {:min-height "500px"}}
+       [sources-dropdown code-block-idx selected]
+       [:iframe
+        {:srcDoc (->> @selected
+                      (map (fn [idx] (code-block-html (nth @all-code-blocks idx))))
+                      (string/join "\n"))
+         :style {:position         "relative"
+                 :width            "100%"
+                 :min-height       "500px"
+                 :border           "1px solid steelblue"
+                 :border-radius    "5px"
+                 :background-color "white"}}]])))
 
 ;;; (3) Vanilla JS to add a button element to each code block which injects the
 ;;; above react component.
 
-(defn create-rendergpt-ele [parent]
-  (let [src-code (code-block-source parent)
-        container (crate/html [:div {:style {:display "none"}}])]
-    (js/console.log "Code:" src-code)
+(defn create-rendergpt-ele [parent idx]
+  (let [container (crate/html [:div {:style {:display "none"}}])]
     (.appendChild parent container)
-    (rdom/render [rendergpt src-code] container)
+    (rdom/render [rendergpt idx] container)
     container))
 
-(defn toggle-render-fn [ele render-button]
+(defn toggle-render-fn [ele idx render-button]
   (let [rendered? (atom false)
         hide! (fn [e] (set! (-> e .-style .-display) "none"))
         show! (fn [e] (set! (-> e .-style .-display) "block"))]
@@ -79,7 +101,7 @@
             content (aget children 1)
             rgpt (if (>= (.-length children) 3)
                    (aget children 2)
-                   (create-rendergpt-ele ele))]
+                   (create-rendergpt-ele ele idx))]
         (if @rendered?
           (do
             (hide! rgpt)
@@ -94,14 +116,20 @@
 
 (defn on-mutation [mutation-records _observer]
   (js/console.log "Mutation observed" mutation-records)
-  (doseq [ele (get-gpt-response-code-blocks)]
-    (when (= (code-block-type ele) "html")
-      (set-code-block-type! ele "html+")
+  (let [n-registered (count @all-code-blocks)]
+    (doseq [[idx ele] (map-indexed vector (get-gpt-response-code-blocks))]
 
-      (let [render-button (crate/html [:button.flex.ml-auto.gap-2 "Render"])
-            toggle-render (toggle-render-fn ele render-button)]
-        (.addEventListener render-button "click" toggle-render)
-        (add-to-code-block-title-bar! ele render-button)))))
+      (when (>= idx n-registered)
+        (swap! all-code-blocks conj
+          {:code (code-block-source ele) :type (code-block-type ele)}))
+
+      (when (= (code-block-type ele) "html")
+        (set-code-block-type! ele "html+")
+
+        (let [render-button (crate/html [:button.flex.ml-auto.gap-2 "Render"])
+              toggle-render (toggle-render-fn ele idx render-button)]
+          (.addEventListener render-button "click" toggle-render)
+          (add-to-code-block-title-bar! ele render-button))))))
 
 (defn register-on-mutation [f]
   (let [observer (js/MutationObserver. f)]
